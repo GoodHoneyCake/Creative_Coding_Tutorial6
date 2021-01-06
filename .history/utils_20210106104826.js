aa8552ba45c20c62b441980ca34a2c659f5606ad
kim <beomsu9696@daum.net>
2021-01-06 10:49:28 +0900

    add hslToHex f

---
+++ .history/utils_20210106104826.js
@@ -0,0 +1,15 @@
+export function distance(x1, y1, x2, y2) {
+  const x = x2 - x1;
+  const y = y2 - y1;
+  return Math.sqrt(x * x + y * y);
+}
+
+export function pointCircle(px, py, cx, cy, r) {
+  if (distance(px, py, cx, cy) <= r) {
+    return ture;
+  } else {
+    return false;
+  }
+}
+
+export function hslToHex(h, s, l) {}
